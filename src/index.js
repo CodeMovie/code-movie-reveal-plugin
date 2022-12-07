492b78a367818ac72ad99f19a6971f67da8defe4
Peter Kröner <peter@peterkroener.de>
2022-12-07 01:53:41 +0100

    Augment animations that are added after init, cleanup after animations that get deleted

--- src/index.js
+++ src/index.js
@@ -1,21 +1,28 @@
 import "@codemovie/code-movie-runtime";
 
+// Associates fragments with "their" animation elements to make teardown logic
+// possible.
+const fragmentMap = new WeakMap();
+
 // For every non-0-frame, add a fragment directly after the animation element.
 // Fragment events ensure that, when one of the fragments becomes visible, the
 // animation gets moved to the corresponding frame.
 function augmentAnimation(animation) {
+  const fragmentList = [];
   for (let i = 0; i < animation.maxFrame; i++) {
     const fragment = document.createElement("span");
-    fragment.className = `fragment animation-plugin-fragment`;
+    fragment.className = `fragment code-movie-plugin-fragment`;
     animation.after(fragment);
+    fragmentList.push(fragment);
   }
+  fragmentMap.set(animation, fragmentList);
 }
 
 // Augment all the animations in a given slide with fragments
 function augmentSlide(slide, deck) {
   const animations = slide.querySelectorAll("code-movie-runtime");
   for (const animation of animations) {
-    augmentAnimation(animation, deck);
+    augmentAnimation(animation);
     deck.syncFragments();
   }
 }
@@ -26,7 +33,7 @@ function getAnimationTargetForFragment(fragment, index) {
   if (!fragment) {
     return null;
   }
-  if (fragment.matches(".animation-plugin-fragment")) {
+  if (fragment.matches(".code-movie-plugin-fragment")) {
     return getAnimationTargetForFragment(
       fragment.previousElementSibling,
       index + 1
@@ -38,20 +45,59 @@ function getAnimationTargetForFragment(fragment, index) {
   return null;
 }
 
-// For plugin for ESM users
+// Augment animations that are added after init (eg. by slowly-loading embed
+// codes) and, while we are at it, also add cleanup logic that removes fragments
+// when "their" respective animation goes away.
+function observeAnimationAdditionsAndRemovals(deck) {
+  new MutationObserver((records) => {
+    for (const record of records) {
+      if (record.type === "childList") {
+        for (const addedNode of record.addedNodes) {
+          if (
+            addedNode.nodeType === 1 &&
+            addedNode.matches("code-movie-runtime")
+          ) {
+            augmentAnimation(addedNode);
+            deck.syncFragments();
+          }
+        }
+        for (const removedNode of record.removedNodes) {
+          if (
+            removedNode.nodeType === 1 &&
+            removedNode.matches("code-movie-runtime")
+          ) {
+            const fragments = fragmentMap.get(removedNode) ?? [];
+            for (const fragment of fragments) {
+              fragment.remove();
+            }
+            deck.syncFragments();
+          }
+        }
+      }
+    }
+  }).observe(document.querySelector(".slides"), {
+    subtree: true,
+    childList: true,
+  });
+}
+
+// Expose the plugin function for ESM users
 export function CodeMovie() {
   return {
     id: "code-movie",
     init: (deck) => {
-      // Augment all slides with animations on init (and after the runtime
-      // element has been registered) by adding extra fragments to each runtime
-      // element.
+      // Augment all slides with existing animations on init (and after the
+      // runtime element has been registered) by adding extra fragments to each
+      // runtime element. Afterwards, start an observer that augments newly
+      // added animations and handles the teardown for removed animations.
       deck.on("ready", async () => {
         await window.customElements.whenDefined("code-movie-runtime");
         for (const slide of deck.getSlides()) {
           augmentSlide(slide, deck);
         }
+        observeAnimationAdditionsAndRemovals(deck);
       });
+
       // Intercepts fragment events and update the corresponding animation's
       // current frame depending on which fragment just became visible or
       // invisible.
@@ -63,6 +109,7 @@ export function CodeMovie() {
         const [animation, index] = target;
         animation.current = index;
       });
+
       deck.on("fragmenthidden", (evt) => {
         const target = getAnimationTargetForFragment(evt.fragment, -1);
         if (!target) {
